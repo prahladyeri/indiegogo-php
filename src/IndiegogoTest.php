dd33e24d58b0ab64dfb4dccc6318956b01b8ff5b
Prahlad Yeri <prahladyeri@yahoo.com>
2015-12-05 03:56:11 +0530

    updates

--- src/IndiegogoTest.php
+++ src/IndiegogoTest.php
@@ -5,16 +5,17 @@
 * @version: 1.3
  * */
  
-require_once('indiegogo.php');
+require_once('Indiegogo.php');
+use Indiegogo\Loader; 
 
-$data = indie_get_comments('e88bd0a00305bfdfb18003a75665643b');
+$data = Loader::indie_get_comments('e88bd0a00305bfdfb18003a75665643b');
 echo $data->count . " comments fetched!\n";
 /*foreach($data->response as $comment){
 	echo $comment->id, $comment->created_at, $comment->text,"\n";
 	echo "\n";
 }*/
 
-$data =  indie_get_contributions('e88bd0a00305bfdfb18003a75665643b');
+$data =  Loader::indie_get_contributions('e88bd0a00305bfdfb18003a75665643b');
 echo $data->count . " contributions fetched!\n";
 foreach($data->response as $contrib){
 	echo $contrib->id, $contrib->created_at, $contrib->amount, $contrib->by,"\n";
